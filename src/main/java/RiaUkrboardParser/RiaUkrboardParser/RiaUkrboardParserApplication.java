186b433849033f6b7b900df2dadc44d927a6ff69
Armarodus <andrew.mikolyk@gmail.com>
2018-09-11 17:07:23 +0300

    first commit

---
+++ src/main/java/RiaUkrboardParser/RiaUkrboardParser/RiaUkrboardParserApplication.java
@@ -0,0 +1,12 @@
+package RiaUkrboardParser.RiaUkrboardParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiaUkrboardParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiaUkrboardParserApplication.class, args);
+	}
+}
---
+++ src/test/java/RiaUkrboardParser/RiaUkrboardParser/RiaUkrboardParserApplicationTests.java
@@ -0,0 +1,16 @@
+package RiaUkrboardParser.RiaUkrboardParser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RiaUkrboardParserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
